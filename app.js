ccd92cc0fe6f14c4afa911ce306980cdbdda711b
Joao Victor <jovickdj@live.com>
2023-03-09 15:16:26 -0500

    Commit 2

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ const bodyPerser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.send("Hello");
+    
+var today = new Date();
+var currentDay = today.getDay();
+var day = "";
+
+if (currentDay === 6 || currentDay === 0) {
+    day = "Weekdend";
+} else {
+    day = "Weekday";
+}
+
+res.render("list", {kindOfday: day});
+
 });
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
